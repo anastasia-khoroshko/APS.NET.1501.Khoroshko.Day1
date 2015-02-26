f0e412dd0c8bb3a9edd1cc567ca62777939ddb5c
anastasia-khoroshko <anastasia.khoroshko@gmail.com>
2015-02-27 01:02:52 +0300

    Task1
    Implement Task1

---
+++ SqrtNPower/NPowerRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqrtNPower
+{
+    public static class NPowerRoot
+    {
+        public static double MethodNewton(double number, int power, double accurancy)
+        {
+            double tempAccurancy = 1.0, tempX = 0, x = 1.0;
+            if (number < 0) return double.NaN;
+            else
+            {
+                while (tempAccurancy > accurancy)
+                {
+                    tempX = ((power - 1) * x + number / Math.Pow(x, power - 1)) / power;
+                    tempAccurancy = Math.Abs(x - tempX);
+                    x = tempX;
+                }
+                return x;
+            }
+        }
+
+        public static double StantardMethod(double number, int power)
+        {
+            return Math.Pow(number, Math.Pow(power, -1));
+        }
+    }
+}
---
+++ TestNPower/TestMethodNRoot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNPower
+{
+    [TestClass]
+    public class TestMethodNRoot
+    {
+        [TestMethod]
+        public void TestCompareNewtonStandart()
+        {
+            double result = SqrtNPower.NPowerRoot.MethodNewton(27, 3, 0);
+            Assert.AreEqual(result, SqrtNPower.NPowerRoot.StantardMethod(27, 3));
+        }
+        [TestMethod]
+        public void TestMethodNewton()
+        {
+            double result = SqrtNPower.NPowerRoot.MethodNewton(-27, 3, 0.001);
+            Assert.AreEqual(result, Double.NaN);
+        }
+
+        [TestMethod]
+        public void TestStandartMethod()
+        {
+            Assert.AreEqual(SqrtNPower.NPowerRoot.StantardMethod(2, 2), 1.4142135623730951);
+        }
+        [TestMethod]
+        public void TestNewton()
+        {
+            Assert.AreEqual(SqrtNPower.NPowerRoot.MethodNewton(2, 2, 0.001), 1.4142135623746899);
+        }
+    }
+}
