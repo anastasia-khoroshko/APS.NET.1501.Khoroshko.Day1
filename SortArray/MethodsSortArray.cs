6a99fba141a709d1f256caa5e4ca10dbc84619ed
anastasia-khoroshko <anastasia.khoroshko@gmail.com>
2015-02-27 23:05:57 +0300

    Task2
    Implement Task2

---
+++ SortArray/MethodsSortArray.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArray
+{
+    public static class MethodsSortArray
+    {
+        public delegate int ComparisonArray(int[] x, int[] y);
+
+        public static int [][] IncreaseSort(int[][] array, ComparisonArray ca)
+        {
+            for(int i=0;i<array.Length;i++)
+            {
+                for(int j=i;j<array.Length;j++)
+                {
+                    if(ca(array[i],array[j])==1)
+                    {
+                        int[] temp=new int[array[i].Length];
+                        array[i].CopyTo(temp,0);
+                        array[i] = new int[array[j].Length];
+                        array[j].CopyTo(array[i], 0);
+                        array[j] = new int[temp.Length];
+                        temp.CopyTo(array[j], 0);
+                    }
+                }
+            }
+            return array;
+        }
+
+        public static  int[][] DecreaseSort(int[][] array, ComparisonArray ca)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (ca(array[i], array[j]) == -1)
+                    {
+                        int[] temp = new int[array[i].Length];
+                        array[i].CopyTo(temp, 0);
+                        array[i] = new int[array[j].Length];
+                        array[j].CopyTo(array[i], 0);
+                        array[j] = new int[temp.Length];
+                        temp.CopyTo(array[j], 0);
+                    }
+                }
+            }
+            return array;
+        }
+        public static int CompareSum(int[] x,int[] y)
+        {
+            if (x.Select(s => s).Sum() > y.Select(s => s).Sum())
+                return 1;
+            else if (x.Select(s => s).Sum() < y.Select(s => s).Sum())
+                return -1;
+            else return 0;
+        }
+
+
+        public static int CompareMinElement(int[] x, int[] y)
+        {
+            if (x.Select(s => s).Min() > y.Select(s => s).Min())
+                return 1;
+            else if (x.Select(s => s).Min() < y.Select(s => s).Min())
+                return -1;
+            else return 0;
+        }
+
+        public static int CompareMaxElement(int[] x, int[] y)
+        {
+            if (x.Select(s => s).Max() > y.Select(s => s).Max())
+                return 1;
+            else if (x.Select(s => s).Max() < y.Select(s => s).Max())
+                return -1;
+            else return 0;
+        }
+
+    }
+}
---
+++ TestSortArray/TestMethodSortArray.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace TestSortArray
+{
+    [TestClass]
+    public class TestSort
+    {
+        [TestMethod]
+        public void TestSortBySumIncrease()
+        {
+            int[][] a = new int[3][];
+            a[0] = new int[] { 20, 6, 12, -5, 7 };
+            a[1] = new int[] { 1, 2, 9 };
+            a[2] = new int[] { 2, 11, 0, -4 };
+            int[][] b = new int[3][];
+            b[0] = new int[] { 2, 11, 0, -4 };
+            b[1] = new int[] { 1, 2, 9 };
+            b[2] = new int[] { 20, 6, 12, -5, 7 };
+            Assert.ReferenceEquals(SortArray.MethodsSortArray.IncreaseSort(a, new SortArray.MethodsSortArray.ComparisonArray(SortArray.MethodsSortArray.CompareSum)), b);
+        }
+        [TestMethod]
+        public void TestSortBySumDecrease()
+        {
+            int[][] a = new int[3][];
+            a[0] = new int[] { 20, 6, 12, -5, 7 };
+            a[1] = new int[] { 1, 2, 9 };
+            a[2] = new int[] { 2, 11, 0, -4 };
+            int[][] b = new int[3][];
+            b[2] = new int[] { 2, 11, 0, -4 };
+            b[1] = new int[] { 1, 2, 9 };
+            b[0] = new int[] { 20, 6, 12, -5, 7 };
+            Assert.ReferenceEquals(SortArray.MethodsSortArray.IncreaseSort(a, new SortArray.MethodsSortArray.ComparisonArray(SortArray.MethodsSortArray.CompareSum)), b);
+        }
+
+        [TestMethod]
+        public void TestSortByMaxElemIncrease()
+        {
+            int[][] a = new int[3][];
+            a[0] = new int[] { 20, 6, 12, -5, 7 };
+            a[1] = new int[] { 1, 2, 9 };
+            a[2] = new int[] { 2, 11, 0, -4 };
+            int[][] b = new int[3][];
+            b[0] = new int[] { 1, 2, 9 };
+            b[1] = new int[] { 2, 11, 0, -4 };            
+            b[2] = new int[] { 20, 6, 12, -5, 7 };
+            Assert.ReferenceEquals(SortArray.MethodsSortArray.IncreaseSort(a, new SortArray.MethodsSortArray.ComparisonArray(SortArray.MethodsSortArray.CompareMaxElement)), b);
+        }
+
+        [TestMethod]
+        public void TestSortByMaxElemDecrease()
+        {
+            int[][] a = new int[3][];
+            a[0] = new int[] { 20, 6, 12, -5, 7 };
+            a[1] = new int[] { 1, 2, 9 };
+            a[2] = new int[] { 2, 11, 0, -4 };
+            int[][] b = new int[3][];
+            b[2] = new int[] { 1, 2, 9 };
+            b[1] = new int[] { 2, 11, 0, -4 };
+            b[0] = new int[] { 20, 6, 12, -5, 7 };
+            Assert.ReferenceEquals(SortArray.MethodsSortArray.DecreaseSort(a, new SortArray.MethodsSortArray.ComparisonArray(SortArray.MethodsSortArray.CompareMaxElement)), b);
+        }
+
+        [TestMethod]
+        public void TestSortByMinElemIncrease()
+        {
+            int[][] a = new int[3][];
+            a[0] = new int[] { 20, 6, 12, -5, 7 };
+            a[1] = new int[] { 1, 2, 9 };
+            a[2] = new int[] { 2, 11, 0, -4 };
+            int[][] b = new int[3][];
+            b[2] = new int[] { 1, 2, 9 };
+            b[1] = new int[] { 2, 11, 0, -4 };
+            b[0] = new int[] { 20, 6, 12, -5, 7 };
+            Assert.ReferenceEquals(SortArray.MethodsSortArray.IncreaseSort(a, new SortArray.MethodsSortArray.ComparisonArray(SortArray.MethodsSortArray.CompareMinElement)), b);
+        }
+
+        [TestMethod]
+        public void TestSortByMinElemDecrease()
+        {
+            int[][] a = new int[3][];
+            a[0] = new int[] { 20, 6, 12, -5, 7 };
+            a[1] = new int[] { 1, 2, 9 };
+            a[2] = new int[] { 2, 11, 0, -4 };
+            int[][] b = new int[3][];
+            b[0] = new int[] { 1, 2, 9 };
+            b[1] = new int[] { 2, 11, 0, -4 };
+            b[2] = new int[] { 20, 6, 12, -5, 7 };
+            Assert.ReferenceEquals(SortArray.MethodsSortArray.DecreaseSort(a, new SortArray.MethodsSortArray.ComparisonArray(SortArray.MethodsSortArray.CompareMinElement)), b);
+        }
+    }
+}
