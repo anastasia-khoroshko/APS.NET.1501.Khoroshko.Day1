f9d54d7dec6e8147bf215102bf0e08a2c05eeb5b
anastasia-khoroshko <anastasia.khoroshko@gmail.com>
2015-02-28 22:20:18 +0300

    Task3
    Implement Task3

---
+++ HexFormatProvider/HexFormat.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexFormatProvider
+{
+    public class HexFormat: IFormattable
+    {
+        private int tempNumber;
+        public HexFormat(int number)
+        {
+            this.tempNumber = number;
+        }
+
+        public string Hex
+        {
+            get
+            {
+                string result = "";
+                double temp=tempNumber;
+                while(temp>=16)
+                {
+                    result = ConvertToHex(temp) + result;
+                    temp = Math.Floor(temp / 16);             
+                }
+                
+                return ConvertToHex(temp)+result;
+            }
+        }
+
+        private string ConvertToHex(double temp)
+        {
+            if (temp % 16 == 10)
+                return "A";
+            else if (temp % 16 == 11)
+                return "B";
+            else if (temp % 16 == 12)
+                return "C";
+            else if (temp % 16 == 13)
+                return "D";
+            else if (temp % 16 == 14)
+                return "E";
+            else if (temp % 16 == 15)
+                return "F";
+            else return (temp % 16).ToString();
+        }
+        public override string ToString()
+        {
+            return this.ToString("h", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format)
+        {
+            return this.ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider provider)
+        {
+            if (String.IsNullOrEmpty(format)) format = "h";
+            if (provider == null) provider = CultureInfo.CurrentCulture;
+            return Hex.ToString(provider) + "h";                
+        }
+    }
+}
---
+++ TestHexFormatProvider/TestHexFormat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace TestHexFormatProvider
+{
+    [TestClass]
+    public class TestHexFormat
+    {
+        [TestMethod]
+        public void TestHex1()
+        {
+            HexFormatProvider.HexFormat number = new HexFormatProvider.HexFormat(46877);
+            Assert.AreEqual(String.Format(number.ToString("h", CultureInfo.CurrentCulture)), "B71Dh");
+        }
+
+        [TestMethod]
+        public void TestHex2()
+        {
+            HexFormatProvider.HexFormat number = new HexFormatProvider.HexFormat(1978);
+            Assert.AreEqual(String.Format(number.ToString("h")), "7BAh");
+        }
+
+        [TestMethod]
+        public void TestHex3()
+        {
+            HexFormatProvider.HexFormat number = new HexFormatProvider.HexFormat(156);
+            Assert.AreEqual(String.Format(number.ToString()), "9Ch");
+        }
+    }
+}
